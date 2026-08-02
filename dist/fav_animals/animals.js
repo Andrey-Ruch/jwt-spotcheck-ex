cfc9e8b5da5b46fba2b96a04e3d681a02598f4b8
Andrey Ruch <andreyruch1@gmail.com>
2026-08-02 20:21:26 +0300

    Update animal sound retrieval to fetch favorite animals and display response dynamically

--- dist/fav_animals/animals.js
+++ dist/fav_animals/animals.js
@@ -1,18 +1,15 @@
 $(document).ready(function () {
-  $(".animal button").on("click", function () {
-    var animal = $(this).siblings("img").attr("alt").toLowerCase();
-
+  $(".getFavAnimal").on("click", function () {
     $.ajax({
-      url: `/sounds/animals/${animal}`,
+      url: `/favorites/animals`,
       method: "GET",
       headers: {
         Authorization: `Bearer ${localStorage.getItem("token")}`,
       },
       success: function (response) {
-        alert(response);
+        $(".animal").append(`<h2>${response.animal}!</h2>`);
       },
       error: function (res, status, error) {
-        alert(res.responseText);
         location.href = "/";
       },
     });
